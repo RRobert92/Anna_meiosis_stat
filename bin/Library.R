74a929b74e405b4eafba9fc90379d14dfc79d27f
RRobert92 <robert.kieiwsz@gmail.com>
2020-12-25 19:40:09 +0100

    Moved functions to bin/

---
+++ bin/Library.R
@@ -0,0 +1,18 @@
+################################################################################
+# Module Library
+#
+# (c) 2020 Schwarz/Kiewisz
+# This code is licensed under GPL V3.0 license (see LICENSE.txt for details)
+#
+# Author: Anna Schwarz & Robert Kiewisz 
+# Created: 2020-12-23
+################################################################################
+
+# Libraries needed for loading of a data
+library(readxl)
+library(tidyverse)
+
+# Libraries needed for plotting of a data
+library(ggplot2)
+library(ggbeeswarm)
+library(Hmisc)
